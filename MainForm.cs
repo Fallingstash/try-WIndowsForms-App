c5a58d2bf5f88d34e2d5f574a484efbde1a6f896
Fallingstash <sashkaprog228@gmail.com>
2025-04-24 16:07:58 +0700

    create form and  link methods

---
+++ MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderSyncApp {
+  public partial class MainForm : Form {
+    public MainForm() {
+      InitializeComponent();
+    }
+
+    private void textSource_TextChanged(object sender, EventArgs e) {
+
+    }
+
+    private void btnBrowseSource_Click(object sender, EventArgs e) {
+      FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+      if (folderDialog.ShowDialog() == DialogResult.OK) {
+        textSource.Text = folderDialog.SelectedPath;
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e) { // не смог переименовать в btnBrowseTarget_Click, всё ломается..
+      FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+      if (folderDialog.ShowDialog() == DialogResult.OK) {
+        textTarget.Text = folderDialog.SelectedPath;
+      }
+    }
+
+    private void btnSync_Click(object sender, EventArgs e) {
+      txtLog.AppendText("Синхронизация начата!" + Environment.NewLine);
+      FileSync.CopyFiles(textSource.Text, textTarget.Text, msg => txtLog.AppendText(msg + Environment.NewLine));
+      txtLog.AppendText("Синхронизация завершена!" + Environment.NewLine);
+    }
+  }
+}
